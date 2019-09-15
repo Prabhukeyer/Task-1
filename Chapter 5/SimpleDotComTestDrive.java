62dc5004bb10cf72703ada2463220491fd62ef32
Prabhu Prakash N <55079227+Prabhukeyer@users.noreply.github.com>
2019-09-15 16:21:09 +0530

    Add files via upload

---
+++ Chapter 5/SimpleDotComTestDrive.java
@@ -0,0 +1,16 @@
+public class SimpleDotComTestDrive {
+
+    public static void main (String[] args) {
+
+        SimpleDotcom dot = new SimpleDotcom();
+
+        int[] locations = {2, 3 ,4};
+
+        dot.setLocationCells(locations);
+
+        String userGuess = "2";
+
+        String result = dot.checkYourself(userGuess);
+
+    }
+}
